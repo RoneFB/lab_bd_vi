bde0d5bacfee84eff465ce6660d294b4919afd02
rone.bento <rone.bento@vmarket.com.br>
2021-03-29 08:09:43 -0300

    Commit inicial - RED TEST

---
+++ pratica_01/src/main/java/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    
+    public static void main(String[] args) {}
+    public int multiplicar(int n1, int n2){
+        return(n1*n2);
+    }
+}
---
+++ pratica_01/src/test/java/CalculadoraTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTest {
+  
+        @Test
+        public void multiplicarTest(){
+            Calculadora calc = new Calculadora();
+            int resultado = calc.multiplicar(3,5);
+            assertEquals(18, resultado);
+        }
+
+
+}
