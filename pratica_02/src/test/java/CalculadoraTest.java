acadd0486e8c3e53095c22cf56e255fd905a1214
rone.bento <rone.bento@vmarket.com.br>
2021-05-21 09:37:07 -0300

    intervalo

--- pratica_02/src/test/java/CalculadoraTest.java
+++ pratica_02/src/test/java/CalculadoraTest.java
@@ -10,7 +10,8 @@ public class CalculadoraTest {
         @Test
         public void multiplicarTest(){
             /* MÃ©todo de teste, executa o multiplicar e compara o resultado esperado 
-             
+            *   Intervalo de numeros inteiros 0 - 5
+            *   
         	*	Resultando o intervalo temos três partições de equivalência: 
         	*	1. onde o produto de dois numeros inteiros negativos OU os dois inteiros positivos -> saida de um inteiro positivo: 
         	*	|Ex: |2 * 2 = 4 | -2 * -2 = 4 |
